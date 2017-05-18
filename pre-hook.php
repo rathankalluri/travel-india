7e219b2475f145df43357782f10860cf93a6fe17
rathankalluri <ratan.kalluri@gmail.com>
2017-05-18 14:53:40 +0530

    Update pre-hook.php

--- pre-hook.php
+++ pre-hook.php
@@ -48,7 +48,7 @@ function processPlacesMessage($update) {
 
 function processDetailsMessage($update) {
 	
-	$url = 'http://rathankalluri.com/tr-in/hook.php?detail=true';
+	$url = 'http://rathankalluri.com/tr-in/hook.php?getdetails=true';
 	$data = array(
 			"detail" => $update["result"]["resolvedQuery"],
 			"source" => $update["result"]["source"]
